fb8fd371fad1ae95bcb4c68b27259bc629fe8e61
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-05-03 17:54:23 +0900

    "[Add]Daoクラスの作成"

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,12 +8,4 @@ CREATE TABLE inquiry
    PRIMARY KEY(id)
 );
 
-CREATE TABLE survey
-(
-   id INT NOT NULL AUTO_INCREMENT,
-   age INT NOT NULL,
-   satisfaction INT NOT NULL,
-   comment VARCHAR(100),
-   created DATETIME NOT NULL,
-   PRIMARY KEY(id)
-);
+
