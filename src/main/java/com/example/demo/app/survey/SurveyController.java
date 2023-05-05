81e1c304ca8b9411c6662026dd01d92970976a1d
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-05-06 06:15:50 +0900

    "[Add]serviceクラス作成、コントローラー編集"

--- src/main/java/com/example/demo/app/survey/SurveyController.java
+++ src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,8 @@
 package com.example.demo.app.survey;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,10 +12,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Survey;
+import com.example.demo.service.SurveyService;
+
 @Controller
 @RequestMapping("/survey")
 
 public class SurveyController {
+	
+	private final SurveyService surveyService;
+	
+	@Autowired
+	public SurveyController(SurveyService surveyService) {
+		this.surveyService = surveyService;
+	}
 	
 	@GetMapping("/form")
 	public String form (SurveyForm surveyForm,Model model) {
@@ -47,6 +60,14 @@ public class SurveyController {
 			model.addAttribute("title", "SurveyForm");
 			return "survey/form";
 		}
+		
+		Survey survey = new Survey();
+		survey.setAge(surveyForm.getAge());
+		survey.setSatisfaction(surveyForm.getSatisfaction());
+		survey.setComment(surveyForm.getComment());
+		survey.setCreated(LocalDateTime.now());
+		
+		surveyService.save(survey);
 		redirectAttributes.addFlashAttribute("complete", "Thanks for your cooperation!");
 		return "redirect:/survey/form";
 	}
