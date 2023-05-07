5e4eac8738d50af61fd50a6f5b8a347c29f35545
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-05-08 07:06:57 +0900

    "[Add]form,confirmにbootstrap適応、headerのblock化"

--- src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -62,13 +62,13 @@ public class InquiryController {
 			Model model,
 			@ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+		return "inquiry/form_boot";
 	}
 	
 	@PostMapping("/form")
 	public String formGoBack (InquiryForm inquiryForm, Model model) {
 		model.addAttribute("title", "Inquiry Form");
-		return "inquiry/form";
+		return "inquiry/form_boot";
 	}
 	
 	@PostMapping("/confirm")
@@ -77,10 +77,10 @@ public class InquiryController {
 				Model model) {
 		if (result.hasErrors()) {
 			model.addAttribute("title", "Inquiry Form");
-			return "inquiry/form";
+			return "inquiry/form_boot";
 		}
 		model.addAttribute("title", "Confirm Page");
-		return "inquiry/confirm";
+		return "inquiry/confirm_boot";
 	}
 	
 	@PostMapping("/complete")
@@ -90,7 +90,7 @@ public class InquiryController {
 			RedirectAttributes redirectAttributes) {
 		if(result.hasErrors()) {
 			model.addAttribute("title", "InquiryForm");
-			return "inquiry/form";
+			return "inquiry/form_boot";
 		}
 		
 		Inquiry inquiry = new Inquiry();
